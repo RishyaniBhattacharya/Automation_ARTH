f8c983ecef6954dff7a74d5fb2c2cdc8e55e5301
Dipaditya Das <DipadityaDas@gmail.com>
2020-11-09 21:29:22 +0530

    Chnages to main.py

--- main.py
+++ main.py
@@ -37,36 +37,27 @@ def main():
 		color(7)
 		print('\n\t\t\t    Main Menu')
 		print('\t\t==================================\n')
-		color()
-		menu = ['Linux', 'Docker', 'Hadoop', 'AWS']
-		for i in range(len(menu)):
-			print('\t\t\tEnter {} : {}'.format(i+1, menu[i]))
-		color(2)
-		print('\t\t\tEnter 0 : Go Back')
-		color(1)
-		print('\t\t\tEnter e : To Exit')
-		color(5)
-		choice = input('Enter your choice : ')
-		if choice == '0':
-			from loc_rem import local_remote
-			local_remote()
-		elif choice == '1':
-			from linux import linux_func
-			linux_func(arg, ip)
-		elif choice == '2':
-			from docker import docker_menu
-			docker_menu(arg, ip)
-		elif choice == '3':
-			from hadoop import hadoop_func
-			hadoop_func(arg, ip)
-		elif choice == '4':
-			from aws import aws_func
-			aws_func(arg, ip)
-		elif choice == 'e':
-			clear()
-			color(7)
+		print("""
+    [1] Hadoop 
+    [2] Docker 
+    [3] AWS CLI
+    [4] Basic Linux
+    [5] SSH
+\033[91m	[99] Exit From The Software
+\033[97m """)
+		x = int(input("Choice =>> "))
+		if x == 1:
+			os.system("python3 ./hadoop.py")
+		elif x == 2:
+			os.system("python3 ./docker.py")
+		elif x == 3:
+			os.system("python3 ./aws.py")
+		elif x == 4:
+			os.system("python3 ./basic_linux.py")
+		elif x == 99:
+			os.system("echo 'Hope You Like This :) Cya! Exiting.....' | lolcat -a -d 100")
 			exit()
 		else:
-			print('Invalid choice !')
+			print("Invalid Input")
 
 main()
